62f1028fa40754c9b4420846657458175765b199
Karan Agrawal <karan.agr9034@gmail.com>
2024-10-12 12:12:57 +0000

    basic stuff

--- contracts/src/SchuldFi.sol
+++ contracts/src/SchuldFi.sol
@@ -1,6 +1,76 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
-contract SchuldFi {
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
+contract SchuldFi is Ownable, ERC721 {
+    using SafeERC20 for IERC20;
+    using Strings for uint256;
+
+    error InvalidAmount();
+    error TokenNotWhiteListed();
+
+    struct NFTDetails {
+        uint256 id;
+        address token;
+        uint256 amount;
+    }
+
+    uint256 totalSupply;
+    mapping(address token => bool isWhitelisted) public tokenWhitelist;
+    mapping(uint256 nftId => NFTDetails nftDetails) public nftDetailsMap; 
+    
+
+    modifier notZeroAmount(uint256 amount) {
+        if(amount==0) {
+            revert InvalidAmount();
+        }
+        _;
+    }
+    modifier isWhitelistedToken(address token) {
+        if(!tokenWhitelist[token]) {
+            revert TokenNotWhiteListed();
+        }
+        _;
+    }
+
+
+    constructor(address _owner) Ownable(_owner) ERC721("SchuldFi lpNFT", "SCHD-LPNFT-" ) {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "";
+    }
+
+    function addLiquidity() external payable notZeroAmount(msg.value) {
+        _addLiquidity(address(0), msg.value);
+    }
+
+    function addLiquidity(address _token, uint256 _amount) external notZeroAmount(_amount) {
+        _addLiquidity(_token, _amount);
+    }
+
+    function removeLiquidity(uint256 _tokenId, uint256 _amount) external notZeroAmount(_amount) {
+        address owner = _ownerOf(_tokenId);
+        _checkAuthorized(owner, _msgSender(), _tokenId);
+        _removeLiquidity(_tokenId, _amount);
+    }
+
+    function _addLiquidity(address _token, uint256 _amount) internal {
+        totalSupply = totalSupply+1;
+        NFTDetails storage nft = nftDetailsMap[totalSupply];
+        nft.id = totalSupply;
+        nft.token = _token;
+        nft.amount = _amount;
+        
+        _mint(_msgSender(), totalSupply);
+        IERC20(_token).safeTransferFrom(_msgSender(), address(this), _amount);
+    }
+
+    function _removeLiquidity(uint256 _tokenId, uint256 _amount) internal {
+        
+    }
 }
---
+++ contracts/src/SchuldVault.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SchuldVault is Ownable {
+    
+
+    constructor(address _owner) Ownable(_owner) {}
+
+}
